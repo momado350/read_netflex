e23e0ae6fd98d92f0ffce5ece95de458d050aa3a
Mohammed Ahmed <mohammedabdalla38@yahoo.com>
2020-08-24 17:21:56 -0500

    checked for video

--- netflex/netflix.py
+++ netflex/netflix.py
@@ -9,3 +9,25 @@ video = input("What show or movie are you looking for? ")
 csvpath = os.path.join("..", "Resources", "netflix_ratings.csv")
 
 
+# ------------------------------------------
+# Set variable to check if we found the video
+found = False
+
+# Open the CSV
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Loop through looking for the video
+    for row in csvreader:
+        if row[0] == video:
+            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+
+            #  Set variable to confirm we have found the video
+            found = True
+
+            #  Stop at first results to avoid duplicates
+            break
+
+    # If the video is never found, alert the user
+    if found is False:
+        print("Sorry about this, we don't seem to have what you are looking for!")
